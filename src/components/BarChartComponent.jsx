7da4441a2fcd7a9f4844a66fc0b3b35205f3c9fb
Vijayratnani <95504153+Vijayratnani@users.noreply.github.com>
2024-08-06 18:46:49 +0500

    Font Family to Julius fixes

--- src/components/BarChartComponent.jsx
+++ src/components/BarChartComponent.jsx
@@ -7,10 +7,9 @@ export default function ChartsOverviewDemo() {
       series={[
         { data: [35, 44, 24, 34] },
         { data: [51, 6, 49, 30] },
-        // { data: [15, 25, 30, 50] },
-        // { data: [60, 50, 15, 25] },
+        { data: [0, 0, 0, 0]}
       ]}
-      height={290}
+      height={400}
       xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
       margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
     />
--- src/components/PieChartComponent.jsx
+++ src/components/PieChartComponent.jsx
@@ -1,4 +1,3 @@
-// src/components/PieChartComponent.jsx
 import React from 'react';
 import { Box, Card, CardContent, Typography } from '@mui/material';
 import { PieChart } from '@mui/x-charts/PieChart';
@@ -9,18 +8,16 @@ const PieChartComponent = () => {
     { name: 'Group B', value: 300 },
     { name: 'Group C', value: 300 },
     { name: 'Group D', value: 200 },
-    { name: 'Group E', value: 100 },
-    { name: 'Group F', value: 50 },
-    { name: 'Group G', value: 25 }
+    { name: 'Group E', value: 150 },
+    { name: 'Group F', value: 100 },
+    { name: 'Group G', value: 75 }
   ];
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', width: 400, height: 400 }}>
-      <Card sx={{ width: '100%', height: '100%',  boxShadow:'none' }}>
-        <CardContent>
-          <Box sx={{ width: '100%', height: 400, boxShadow:'none', fontSize:20, justifyContent: 'center',
-            alignItems:'center'
-           }}> 
+    <Box sx={{ display: 'flex', justifyContent: 'center', width: 400, height: 500 }}> {/* Ensure the height is defined here */}
+      <Card sx={{ width: '100%', height: '100%', boxShadow: 'none' }}>
+        <CardContent sx={{ height: '150%' }}> {/* Ensure the height is defined here */}
+          <Box sx={{ width: '100%', height: '100%', boxShadow: 'none', fontSize: 20, justifyContent: 'center', alignItems: 'center' }}>
             <PieChart 
               series={[
                 {
@@ -35,10 +32,11 @@ const PieChartComponent = () => {
                   cy: 200,
                 }
               ]}
+              sx={{ height: '150%' }} // Ensure the height is defined here
             />
           </Box>
-         </CardContent>
-       </Card>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
